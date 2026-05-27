64b56f8a0c99e694a30da31f36ded776c307d6a8
Sucuri Security <guara.taste@gmail.com>
2026-05-27 19:12:16 -0300

    hash

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"bufio"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"net"
 )
@@ -9,12 +12,31 @@ import (
 const PORT = "8000" 
 const PROTOCOL = "tcp"
 
+type Transaction struct {
+	Value int
+	Origim string
+	Destination string
+	BankAccountNumber string
+}
+
+type Balance struct {
+	BankAccountNumber int
+	NumDeposits int
+	NumWithdraws int
+	TotalBalance int
+
+}
 func main () {
 	ln, err := net.Listen(PROTOCOL, ":" + PORT)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Server running on :8000")
+	
+	json_msg := Transaction{1000, "lucas", "matheus", "123-bank-id"}
+	fmt.Println("without")
+	fmt.Println(json_msg)
+	fmt.Println(GenerateHashFromTransaction(json_msg))
 
 	for {
 		conn, err := ln.Accept()
@@ -25,6 +47,8 @@ func main () {
 
 		go Handle(conn)
 	}
+	
+
 }
 
 func Handle(conn net.Conn) {
@@ -37,8 +61,23 @@ func Handle(conn net.Conn) {
 			fmt.Println("Client disconnected")
 			return
 		}
+
+
 		fmt.Println("Received:", msg)
 		conn.Write([]byte("Echo: " + msg))
 	}
 }
+
+func GenerateHashFromTransaction(t Transaction) string {
+	serialized_transaction, err := json.Marshal(t)
+	if err != nil { 
+		fmt.Println("Can't serialize", serialized_transaction)
+	}
+
+	hash_transaction := sha256.Sum256(serialized_transaction)
+	encoded_hash := hex.EncodeToString(hash_transaction)
+
+	return string(encoded_hash)
+}
+	
 
